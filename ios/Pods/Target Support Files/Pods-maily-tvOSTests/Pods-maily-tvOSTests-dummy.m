8ee58e43041e7bbd02fc8cfb885044c133ca1432
ugupta <ullas.g@smartenspaces.com>
2019-02-20 01:43:52 +0530

    **Changes** : adding appCenter **Developer : Ullas Gupta**

---
+++ ios/Pods/Target Support Files/Pods-maily-tvOSTests/Pods-maily-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maily_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_maily_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-maily/Pods-maily-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maily : NSObject
+@end
+@implementation PodsDummy_Pods_maily
+@end
